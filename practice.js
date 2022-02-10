c19cad218b75377716cb101361f40a4fd5cdfbcc
nazmul huda <nazmul4836@gmail.com>
2022-02-10 18:47:40 +0600

    first commit

---
+++ practice.js
@@ -0,0 +1,87 @@
+//১. কখন const আর কখন let দিয়ে ভেরিয়েবল ডিক্লেয়ার করতে হয় সেটা তোমাকে জানতেই হবে। তুমি নিজে নিজে একটা const দিয়ে আরেকটা let দিয়ে ভেরিয়েবল ডিক্লেয়ার করে ফেলো।
+let number = 12;
+const name = 'nazmul';
+number = number + 10;
+// console.log(number);
+
+//২. টেম্পলেট স্ট্রিং দিয়ে একটা স্ট্রিং তৈরি করো। সেটার মধ্যে উপরে ডিক্লেয়ার করা ভেরিয়েবল এর মান ডাইনামিক ভাবে বসাও। একইভাবে উপরে একটা অবজেক্ট ডিক্লেয়ার করো। এবং ডাইনামিকভাবে উপরের অবজেক্ট এর দুইটা প্রপার্টি এর মান তোমার টেমপ্লেট স্ট্রিং এর মধ্যে বসাও। 
+
+const tsValue = 'nazmul'
+const object = { name: 'hp leptop', price: 10000, color: { black: 'deap', blue: 'light' } }
+const templateString = `
+this is template ${tsValue} string ${object.color.blue}
+complete string template
+
+`
+// console.log(templateString);
+
+//৩.১ একটা প্যারামিটার ওয়ালা arrow ফাংশন ডিক্লেয়ার করো। এবং সেই ফাংশনের কাজ হবে তুমি কোন ইনপুট দিলে সেই ইনপুট সংখ্যাকে ৫ দিয়ে ভাগ করে আউটপুট দিবে। 
+const arrow = number => number / 5;
+const result1 = arrow(10);
+//৩.২ তুমি দুইটা প্যারামিটার ওয়ালা একটা অ্যারো ফাংশন লিখবে। সেই ফাংশনের ভিতরে কাজ হবে। প্রত্যেকটা ইনপুট প্যারামিটার এর সাথে ২ যোগ করবে তারপর যোগফল দুইটা গুণ করবে। ক্যামনে করবে সেটা চিন্তা করে বের করার চেষ্টা করো 
+const arrow2 = (num1, num2) => (num1 + 2) * (num2 + 2);
+const result2 = arrow2(5, 10);
+// console.log(result2);
+//৩.৩ এইবার তিনটা প্যারামিটার ওয়ালা একটা অ্যারো ফাংশন ডিক্লেয়ার করো। যেই ফাংশনের কাজ হবে তিনটা প্যারামিটার নিয়ে সেই তিনটা প্যারামিটারকে গুণ করে সেই রেজাল্ট রিটার্ন করবে। 
+const arrow3 = (num1, num2, num3) => num1 * num2 * num3;
+const result3 = arrow3(2, 3, 4);
+// console.log(result3);
+//৩.৪ এইবার দুইটা প্যারামিটার ওয়ালা একটা অ্যারো ফাংশন নিবে। ওই arrow ফাংশনটা হবে অনেকগুলা লাইনের। সেখানে প্রত্যেকটা ইনপুট প্যারামিটার এর সাথে ২ যোগ করবে তারপর যোগফল দুইটা গুণ করবে। ক্যামনে করবে সেটা চিন্তা করে বের করার চেষ্টা করো। 
+
+const arrow4 = (num1, num2) => {
+    const frist = num1 + 2;
+    const second = num2 + 2;
+    return frist * second;
+}
+const result4 = arrow4(10, 20);
+// console.log(result4);
+
+//৫. অনেকগুলা সংখ্যার একটা array হবে। তারপর তোমার কাজ হবে array এর উপরে map ইউজ করে। প্রত্যেকটা উপাদানকে ৫ দিয়ে গুন্ করে গুনফল আরেকটা array হিসেবে রাখবে। পুরা কাজটা এক লাইনে হবে। 
+const numbers = [2, 3, 4, 5, 6, 7, 8, 9];
+const output = numbers.map(number => number * 5);
+// console.log(output);
+
+//৬. [ চ্যালেঞ্জিং। গুগলে সার্চ দিয়ে বের করো ] অনেকগুলা সংখ্যার একটা array থেকে শুধু বিজোড় সংখ্যা বের করে নিয়ে আসার জন্য filter ইউজ করো 
+const totalNumbers = [2, 3, 4, 5, 6, 7, 8, 9];
+const odd = totalNumbers.filter(number => number % 2 != 0);
+// console.log(odd);
+
+//৭. একটা array এর মধ্যে অনেকগুলা অবজেক্ট আছে। সেখানে যেই অবজেক্ট এর price আছে ৫০০০ টেক্কা সেই অবজেক্টকে find দিয়ে বের করো। 
+const products = [
+    { name: 'phone', price: 7000, color: 'black' },
+    { name: 'watch', price: 5000, color: 'grey' },
+    { name: 'laptop', price: 5000, color: 'green' },
+];
+const find = products.find(product => product.price == 5000);
+// console.log(find);
+
+//৭.৫ [এক্সট্রা] জাভাস্ক্রিপ্ট এ array এর map, forEach, filter, find কোনটা দিয়ে কি হয়। সেটার একটা সামারি লিখে ফেলো। 
+
+//map: from a object do something with all property is called maping.
+//filter: when u put a conditon and after matched the condition filter filtering this value and put on another array.
+//find: after matched a condition took the first matching value.
+
+//৮. সিম্পল একটা জাভাস্ক্রিপ্ট অবজেক্ট এর কোন একটা প্রোপার্টিকে ভেরিয়েবল হিসেবে ডিক্লেয়ার করার জন্য destructuring ইউজ করো। 
+const company = { name: 'kz ltd', addres: 'mirpur', contact: '01688593478' };
+const { addres, contact } = company;
+// console.log(addres, contact);
+
+//৯. [চ্যালেঞ্জিং] array এর destructuring করবে আর সেটা করার জন্য তুমি এরে এর থার্ড পজিশন এর উপাদান কে destructuring করে 'three' নামক একটা ভেরিয়েবল এ রাখবে। 
+const array = [10, 11, 4, 6, 4];
+const [one, two, three] = array;
+// console.log(three);
+
+// ১০. তিনটা প্যারামিটার ওয়ালা একটা ফাংশন লিখবে। যেই ফাংশনের কাজ হবে তিনটা প্যারামিটার নিয়ে সেই তিনটা প্যারামিটার এর যোগ করে যোগফল রিটার্ন করবে। আর থার্ড প্যারামিটার এর একটা ডিফল্ট ভ্যালু থাকবে। সেটা হবে ৭। 
+
+const defaultValue = (num1, num2, num3 = 7) => num1 + num2 + num3;
+const pValue = defaultValue(10, 12);
+console.log(pValue);
+
+
+
+
+
+
+
+
+
